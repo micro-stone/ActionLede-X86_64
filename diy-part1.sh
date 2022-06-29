1da0fcc2288d125c94250f2d6c494c8405b4d6b8
Huadao1108 <huadaoshf@yeah.net>
2022-06-29 09:03:54 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,7 @@
 # Uncomment a feed source
 
 # Add a feed source
+#添加需要的插件源，按照格式填入
+#格式：echo 'src-git <name>  <地址>' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
