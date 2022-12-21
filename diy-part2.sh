a312ec84d14bd49c648e1b68cbfccd71afb42da2
Huadao1108 <huadaoshf@yeah.net>
2022-12-21 10:59:40 +0800

    clean

--- diy-part2.sh
+++
@@ -1,6 +0,0 @@
-
-#修改默认IP地址
-# Modify default IP
-sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
-#修改10.10.10.2为你需要的ip地址
-sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
